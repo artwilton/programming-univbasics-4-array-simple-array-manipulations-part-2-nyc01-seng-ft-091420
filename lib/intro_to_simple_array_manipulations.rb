9e08d1cc5e161f7e89ebf6a54315e070ef6400a0
Arthur Wilton <arthur.m.wilton@gmail.com>
2020-09-04 21:21:12 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -2,11 +2,11 @@ def using_concat(array_1, array_2)
   array_1.concat(array_2)
 end
 
-def using_insert
+def using_insert(array, element)
 
 end
 
-def using_uniq(array, element)
+def using_uniq(array)
   array.uniq
 end
 
